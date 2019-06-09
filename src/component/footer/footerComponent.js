7088cad5ddac4e1c8f2c9d44af8e8f0e79c8ec15
aras armani <aras.armani2014@gmail.com>
2019-06-09 18:03:26 +0300

    fixed all

--- src/component/footer/footerComponent.js
+++ src/component/footer/footerComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './style.css';
-
+import zarrinpal from '../../assets/images/zarrinpal.png'
 
 class FooterComponent extends Component {
     constructor(props) {
@@ -10,8 +10,22 @@ class FooterComponent extends Component {
     render() { 
         return ( 
             <div className="container-fluid footer">
-                <div className="container p-20 txt-center">
-                    <p>شرکت تکنولوژی هوشمند و نامحدود زیگورات کیش - 1398</p>
+                <div className="container p-20 ">
+                  <div className="row">
+                    <div className="col-20">
+                        <div className="zarrinpal">
+                            <img src={zarrinpal} alt="زرین پال" />
+                        </div>
+                    </div>
+                    <div className="col-80">
+                        <p> شرکت تکنولوژی هوشمند و نامحدود زیگورات کیش - 1398 </p>
+                        <div className="social-container">
+                            <div className="social"></div>
+                            <div className="social"></div>
+                            <div className="social"></div>
+                        </div>
+                    </div>
+                  </div>
                 </div>
             </div>
          );
