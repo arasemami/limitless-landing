0c2bb52dd6a62f39bc2480b68d82788b5e4354af
aras armani <aras.armani2014@gmail.com>
2019-06-11 16:22:37 +0300

    limitless coming soon is fixede

---
+++ src/container/comingSoon/comingSoon.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './style.css';
+
+
+
+
+
+
+
+class ComingSoonComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="coming-soon">
+               <p>coming soon ....</p> 
+            </div>
+         );
+    }
+}
+ 
+export default ComingSoonComponent;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './container/index/indexComponent';
+// import App from './container/index/indexComponent';
+import App from './container/comingSoon/comingSoon';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
